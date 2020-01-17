288899d99b98d8b241bf73e78186c612f1b36554
Prestwick97 <prestwick97@gmail.com>
2020-01-17 15:08:56 -0800

    fix spelling error in javascript

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,9 @@ $(document).ready(function() {
   
   $("form#language").on("submit", function(event) {
     event.preventDefault();
-    if("1"&&"a")
+    var animal= $("#animal").val();
+    var shape = $("#shape").val();
+    if(shape === "1" && animal === "a")
     $("#css").show();
     else ($("#java").show());
   });
