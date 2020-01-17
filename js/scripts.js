67204155d244b7e419670773bb92d092d2953b41
Prestwick97 <prestwick97@gmail.com>
2020-01-17 13:32:29 -0800

    fix jquery problem - swithc inputs of scripts.js and jquery.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
-// $(document).ready(function() {
+$(document).ready(function() {
   // $("ol").on("click", "li", function() {
   //   console.log(this);
   //   $(this).next().slideToggle();
+  // }
   
-  // $("form#language").on("submit", function(event) {
-  //   event.preventDefault();
-  //   if("#language" === 1 && "#color" === a)
-  //   $("#css").show();
-  //   else ($("#java").show());
-  // });
+  $("form#language").on("submit", function(event) {
+    event.preventDefault();
+    if("1"==="a")
+    $("#css").show();
+    else ($("#java").show());
+  });
 
-// });
- prompt("Is this working???");
+});
