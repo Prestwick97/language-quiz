3e42cf9c65a748a51d152b86eae7ce0269a1387a
Prestwick97 <prestwick97@gmail.com>
2020-01-17 13:39:19 -0800

    added matrix background to result pop-up

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,11 @@
 $(document).ready(function() {
-  // $("ol").on("click", "li", function() {
-  //   console.log(this);
-  //   $(this).next().slideToggle();
-  // }
+
   
   $("form#language").on("submit", function(event) {
     event.preventDefault();
-    if("1"==="a")
+    if("1"&&"a")
     $("#css").show();
-    else ($("#java").show());
+    else if ($("#java").show());
   });
 
 });
