abcaf43973260351ddd908061d955d66fa084d06
Prestwick97 <prestwick97@gmail.com>
2020-01-17 13:04:12 -0800

    added backgrounf -waves-

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,11 @@ $(document).ready(function() {
   // $("ol").on("click", "li", function() {
   //   console.log(this);
   //   $(this).next().slideToggle();
+  
+  $("form#language").on("submit", function(event) {
+    event.preventDefault();
+    if(1 === a)
+    $("#css").show();
+    else ($("#java").show());
   });
+});
