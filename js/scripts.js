9655b4e4c1e204d33492fb0c907c671641a83f98
Prestwick97 <prestwick97@gmail.com>
2020-01-17 13:20:43 -0800

    copmment out current java besides jquery initiantion and add prompt to troubleshoot javascript

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,12 @@ $(document).ready(function() {
   //   console.log(this);
   //   $(this).next().slideToggle();
   
-  $("form#language").on("submit", function(event) {
-    event.preventDefault();
-    if(1 === a)
-    $("#css").show();
-    else ($("#java").show());
-  });
+  // $("form#language").on("submit", function(event) {
+  //   event.preventDefault();
+  //   if("#language" === 1 && "#color" === a)
+  //   $("#css").show();
+  //   else ($("#java").show());
+  // });
+
 });
+ prompt("Is this working???");
