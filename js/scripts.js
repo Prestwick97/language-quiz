e5d29e897fc1b95cd66a1e25550ffeb730358241
Prestwick97 <prestwick97@gmail.com>
2020-01-17 16:23:41 -0800

    Comment out buggy javascript and removed variables for functionality. Still needs work.

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,14 @@ $(document).ready(function() {
     event.preventDefault();
     var animal= $("#animal").val();
     var shape = $("#shape").val();
-    if(shape === "1" && animal === "a")
-    $("#css").show();
-    else ($("#java").show());
+    var author = $("#author").val();
+    var baby = $("#baby").val();
+    var money = $("#money").val();
+    if("1" && "a" && "i" && "5" && "e")
+    $("#python").show();
+    // else if (shape === "2" && animal === "b" &&author ==="j" && baby ==="6" && money === "f")
+    // $("#java").show();
+    else $("C").show();
   });
 
 });
