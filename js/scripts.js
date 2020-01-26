ddbc856e63457205e4876539debf5fb21820243a
Prestwick97 <prestwick97@gmail.com>
2020-01-26 15:37:09 -0800

    updated if statements to equate to variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,20 @@
-$(document).ready(function() {
+2$(document).ready(function() {
 
   
   $("form#language").on("submit", function(event) {
     event.preventDefault();
-    var animal= $("#animal").val();
     var shape = $("#shape").val();
+    var animal= $("#animal").val();
     var author = $("#author").val();
     var baby = $("#baby").val();
     var money = $("#money").val();
-    if("1" && "a" && "i" && "5" && "e")
+    if(shape == "1" && animal == "a" && author == "i" && baby == "5" && money == "e")
     $("#python").show();
     // else if (shape === "2" && animal === "b" &&author ==="j" && baby ==="6" && money === "f")
     // $("#java").show();
-    else if("2" && "b" && "j" && "6" && "f")
+    else if(shape == "2" && animal == "b" && suthor == "j" && baby == "6" && money == "f")
     $("C").show();
+    
     else $("java").show();
   });
 
