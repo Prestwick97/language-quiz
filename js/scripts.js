d023a8ce97b2c78a632bcb2254d902d0983de935
Prestwick97 <prestwick97@gmail.com>
2020-01-26 15:42:12 -0800

    add curly brackets

--- js/scripts.js
+++ js/scripts.js
@@ -8,13 +8,17 @@ $(document).ready(function() {
     var author = $("#author").val();
     var baby = $("#baby").val();
     var money = $("#money").val();
-    if(shape == "1" && animal == "a" && author == "i" && baby == "5" && money == "e")
+    if(shape == "1" && animal == "a" && author == "i" && baby == "5" && money == "e") {
     $("#python").show();
- 
-    else if(shape == "2" && animal == "b" && suthor == "j" && baby == "6" && money == "f")
+    }
+    
+    else if(shape == "2" && animal == "b" && suthor == "j" && baby == "6" && money == "f") {
     $("C").show();
+    }
 
-    else $("java").show();
+    else {
+      $("java").show();
+    }
   });
 
 });
