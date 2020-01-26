640ab5005e1edde3fbe2838f51c3db778c8583e9
Prestwick97 <prestwick97@gmail.com>
2020-01-26 15:47:42 -0800

    add # to id tags for result language

--- js/scripts.js
+++ js/scripts.js
@@ -8,16 +8,17 @@ $(document).ready(function() {
     var author = $("#author").val();
     var baby = $("#baby").val();
     var money = $("#money").val();
+
     if(shape == "1" && animal == "a" && author == "i" && baby == "5" && money == "e") {
     $("#python").show();
     }
     
     else if(shape == "2" && animal == "b" && author == "j" && baby == "6" && money == "f") {
-    $("C").show();
+    $("#C").show();
     }
 
     else {
-      $("java").show();
+      $("#java").show();
     }
   });
 
